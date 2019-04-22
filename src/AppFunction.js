10d4d4facb9db7439b7deed1bcb231e4f8878517
deanban <pbp2005@gmail.com>
2019-04-22 00:21:00 -0400

    more useEffects

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -1,24 +1,55 @@
 import React, { useState, useEffect } from 'react';
 
+const initLocationState = {
+  lat: null,
+  lng: null,
+  speed: null
+};
 const App = () => {
   const [count, setCount] = useState(0);
   const [on, setOn] = useState(false);
   const [mousePos, setMousePos] = useState({ x: null, y: null });
   const [status, setStatus] = useState(navigator.onLine);
   // ⬆️☝ that means I can set state directly from props
+  const [location, setLocation] = useState(initLocationState);
+
+  //⬇️ Creating a boolean for cleaning up getCurrentPosition()
+  //since it doesn't have a easy clean up method
+  let mounted = true;
 
   useEffect(() => {
     document.title = `Clicked ${count} Times.`;
     window.addEventListener('mousemove', handleMouseMove);
     window.addEventListener('online', handleOnline);
     window.addEventListener('offline', handleOffline);
+    navigator.geolocation.getCurrentPosition(handleGeolocation);
+    const watchId = navigator.geolocation.watchPosition(handleGeolocation);
 
+    //return to clean up any side effects
+    //sort of like componentWillUnmount()
     return () => {
       window.removeEventListener('mousemove', handleMouseMove);
       window.removeEventListener('online', handleOnline);
       window.removeEventListener('offline', handleOffline);
+      navigator.geolocation.clearWatch(watchId);
+      mounted = false;
     };
+
+    //⬇️ for an empty array useEffect is run twice
+    //on mount and unmount
+    //specifying something e.g. count means
+    //that the side effects rely on count
   }, [count]);
+
+  const handleGeolocation = event => {
+    if (mounted) {
+      setLocation({
+        lat: event.coords.latitude,
+        lng: event.coords.longitude,
+        speed: event.coords.speed
+      });
+    }
+  };
 
   const handleOnline = () => {
     setStatus(true);
@@ -69,6 +100,11 @@ const App = () => {
       <p>
         You are <strong>{status ? 'online' : 'offline'}</strong>
       </p>
+
+      <h2>Geo Location</h2>
+      <p>Lat: {location.lat}</p>
+      <p>Lng: {location.lng}</p>
+      <p>Your Speed is {location.speed ? location.speed : '0'}</p>
     </>
   );
 };
